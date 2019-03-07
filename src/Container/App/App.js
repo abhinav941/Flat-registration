31d381f16801c9be88b55a101433b3434adb43ee
abhi <abhirao941@github.com>
2019-03-08 01:18:17 +0530

    initialize

---
+++ src/Container/App/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <main>
+        <header>Header -> Navigation</header>
+        <sidebar>Sidebar</sidebar>
+        <page>Page</page>
+      </main>
+    );
+  }
+}
+
+export default App;
